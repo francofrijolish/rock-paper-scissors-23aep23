c3d16afad17e5d389e3faceb4a6d5e4703045604
francofrijolish <francomendoza260@gmail.com>
2023-09-27 00:46:00 -0500

    ACTUALIZACION PRUEBA PROYECTO

--- main.js
+++ main.js
@@ -1,50 +1,42 @@
-functioncgetcomputerchoice (){
+function getComputerChoice() {
     const cPselection = ['rock,paper,scisors'];
     const random = math.floor(math.random() * cPselection.length);
 }
 
-function game (){
-const playerselection = prompt("Please chose between'rock")
+function game() {
+    const playerselection = prompt("Please chose between''rock', 'paper' or scissors'");
 
-let playerPoints = 0;
-let computerPoints = 0;
+    let playerPoints = 0;
+    let computerPoints = 0;
 
-function playRound(playerselection, computerselection)
-{
-    if (playerSelection === computerSelection) {
-        console.log(`tie! ${playerSelection}is equal than ${computerselection}`);
-    } else if (
-        (playerSelection === 'scisors'&&  computerSelection === 'paper')
-        (playerSelection === 'paper' && computerSelection === 'rock')
+    function playRound(playerselection, computerselection) {
+        if (playerSelection === computerSelection) {
+            console.log(`tie! ${playerSelection}is equal than ${computerselection}`);
+        } else if (
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+                (playerSelection === 'paper' && computerSelection === 'rock')
 
-        (playerSelection === 'rock' && computerSelection ==='scisors')) {
-            playerPoints==;
+                (playerSelection === 'rock' && computerSelection === 'scisors')) {
+            playerPoints++;
             console.log(`You win! $ {playerSelection} beats $ { computerSelection}`);
         } else {
-            computerPoints==;
+            computerPoints++;
             console.log(`You lose! ${playerSelection} beats $ {computerSelection}`)
 
             playRound(playerSelection.tolocaLeLowerCase(),
-            computerSelection);
-        }
+                computerSelection);
 
         function gameLoop() {
-            for(let i = 0; i < 10; i++){
+            for (let i = 0; i < 10; i++) {
                 if (playerPoints > 4) {
                     return `Congratulations, you win, haved $ {playerPoints} and computer haved ${computerPoints}`;
-
+                    if (computerPoints > 4) {
+                    return `Game over, you lose, haved $ {playerPoints} and computer haved ${computerPoints}`;
+                    game();
                 }
-                game();
             }
         }
-
+    }
         gameLoop();
-
-
-
-
-
-
-    )
 }
 }
