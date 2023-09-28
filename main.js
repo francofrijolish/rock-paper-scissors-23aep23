3070f064fe62cf8e7da0e101f52a0dc66f9c3024
francofrijolish <francomendoza260@gmail.com>
2023-09-28 00:10:46 -0500

    actualizacion de proyecto

--- main.js
+++ main.js
@@ -1,42 +1,87 @@
+let playerSelection;
+let playerPoints = 0;
+let computerPoints = 0;
+const imgPlayer = document.getElementById('imgPlayerSelection');
+const imgComputer = document.getElementById('imgComputerSelection');
+const resultRound = document.getElementById('resultRound');
+const resultGame = document.getElementById('resultGame');
+
+const selectionRock = document.getElementById('boton1');
+selectionRock.addEventListener('click', () => {
+    playerSelection = 'Rock';
+    imgPlayer.classList.remove('scissors', 'paper');
+    imgPlayer.classList.add('rock');
+    gameLoop();
+    console.log(imgComputer);
+});
+
+const selectionPaper = document.getElementById('boton2');
+selectionPaper.addEventListener('click', () => {
+    playerSelection = 'Paper';
+    imgPlayer.classList.remove('rock', 'scissors');
+    imgPlayer.classList.add('paper');
+    gameLoop();
+});
+
+const selectionScissors = document.getElementById('boton3');
+selectionScissors.addEventListener('click', () => {
+    playerSelection = 'Scissors';
+    imgPlayer.classList.remove('rock', 'paper');
+    imgPlayer.classList.add('scissors');
+    gameLoop();
+});
+
 function getComputerChoice() {
-    const cPselection = ['rock,paper,scisors'];
-    const random = math.floor(math.random() * cPselection.length);
+    const cPselection = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * cPselection.length);
+
+    if(cPselection[random] === 'Scissors'){
+        imgComputer.classList.remove('rock', 'paper');
+        imgComputer.classList.add('scissors');
+    }else if(cPselection[random] === 'Paper'){
+        imgComputer.classList.remove('rock', 'scissors');
+        imgComputer.classList.add('paper');
+    }else if(cPselection[random] === 'Rock'){
+        imgComputer.classList.remove('scissors', 'paper');
+        imgComputer.classList.add('rock');
+    };
+    return cPselection[random];
 }
 
 function game() {
-    const playerselection = prompt("Please chose between''rock', 'paper' or scissors'");
-
-    let playerPoints = 0;
-    let computerPoints = 0;
+    const computerSelection = getComputerChoice();
 
     function playRound(playerselection, computerselection) {
         if (playerSelection === computerSelection) {
-            console.log(`tie! ${playerSelection}is equal than ${computerselection}`);
+            resultRound.innerHTML = (`tie! ${playerSelection}is equal than ${computerselection}`);
         } else if (
-            (playerSelection === 'scissors' && computerSelection === 'paper')
-                (playerSelection === 'paper' && computerSelection === 'rock')
-
-                (playerSelection === 'rock' && computerSelection === 'scisors')) {
+            (playerSelection === 'Scissors' && computerSelection === 'Paper')
+            ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock')
+            ||
+            (playerSelection === 'Rock' && computerSelection === 'Scisors')) {
             playerPoints++;
-            console.log(`You win! $ {playerSelection} beats $ { computerSelection}`);
+            resultRound.innerHTML = (`You win! ${playerSelection} beats ${computerSelection}`);
         } else {
             computerPoints++;
-            console.log(`You lose! ${playerSelection} beats $ {computerSelection}`)
-
-            playRound(playerSelection.tolocaLeLowerCase(),
-                computerSelection);
-
-        function gameLoop() {
-            for (let i = 0; i < 10; i++) {
-                if (playerPoints > 4) {
-                    return `Congratulations, you win, haved $ {playerPoints} and computer haved ${computerPoints}`;
-                    if (computerPoints > 4) {
-                    return `Game over, you lose, haved $ {playerPoints} and computer haved ${computerPoints}`;
-                    game();
-                }
-            }
+            resultRound.innerHTML = (`You lose! ${playerSelection} beats ${computerSelection}`)
         }
     }
-        gameLoop();
+    playRound(playerSelection, computerSelection);
 }
+
+function gameLoop() {
+            
+    if (playerPoints > 4) {
+        resultGame.classList.add('gameWin')
+        resultGame.innerHTML = (`Congratulations, you win haved ${playerPoints} and computer haved ${computerPoints}`);
+        return score;
+    }if (computerPoints > 4) {
+            resultGame.classList.add('gameOver')
+            resultGame.innerHTML = (`Game over, you lose, haved ${playerPoints} and computer haved ${computerPoints}`);
+            return score;
+    }
+    const score = document.getElementById('score');
+    score.innerHTML = `player = ${playerPoints} computer = ${computerPoints}`;
+    game();
 }
